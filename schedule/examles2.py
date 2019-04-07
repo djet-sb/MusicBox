ebc63c0576c935739c9b72bb97653a7444687f23
Sergey Budnikov <s.budnikov@escalar.ru>
2019-04-07 22:51:11 +0300

    add schedule

--- schedule/examles2.py
+++ schedule/examles2.py
@@ -1,4 +1,5 @@
 from playhouse.postgres_ext import *
+import datetime, json
 
 user = 'postgres'
 password = 'Pasd@1234'
@@ -19,4 +20,5 @@ class musicbox(Model):
         database = db  # This model uses the "people.db" database.
 
 musicbox.create_table(True)
-
+musicbox.create(publish_in=datetime.date(2019, 3, 22),
+                publish_data=str({"test":"1223"}))
