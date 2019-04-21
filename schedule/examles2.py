d0f8814c0a4b049b272ed34d5caa32db89cda130
Sergey Budnikov <s.budnikov@escalar.ru>
2019-04-21 14:07:47 +0300

    add examles

--- schedule/examles2.py
+++ schedule/examles2.py
@@ -1,5 +1,5 @@
 from playhouse.postgres_ext import *
-import datetime, json
+import datetime
 
 user = 'postgres'
 password = 'Pasd@1234'
@@ -13,12 +13,19 @@ db = PostgresqlDatabase(
 
 
 class musicbox(Model):
-    publish_in = DateField()
-    published_in = DateField()
+    publish_in = IntegerField()
+    published_in = IntegerField(default=0)
     publish_data = JSONField()
+
     class Meta:
         database = db  # This model uses the "people.db" database.
 
+
+publish_in_unixtime = int(datetime.datetime(2010, 2, 25, 23, 23).timetuple())
+
+# Over
+# datetime.datetime.utcfromtimestamp(publish_in_unixtime)
 musicbox.create_table(True)
-musicbox.create(publish_in=datetime.date(2019, 3, 22),
-                publish_data=str({"test":"1223"}))
+
+musicbox.create(publish_in=publish_in_unixtime,
+                publish_data=str({"test": "1223"}))
